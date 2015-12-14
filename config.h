139eb0be251659686366610ab5df0106cbbf41c9
Rauros Industries <rauros@tfwno.gf>
2015-12-13 19:49:31 -0500

    First commit.

---
+++ config.h
@@ -0,0 +1,12 @@
+static const Bool wmborder = True;
+static const char *font = "-*-*-medium-*-*--11-*-*-*-*-*-iso10646-1";
+static const char *normbgcolor = "#282A2E";
+static const char *normfgcolor = "#C5C8C6";
+static const char *pressbgcolor = "#ffffff";
+static const char *pressfgcolor = "#000000";
+static const char *highlightbgcolor = "#282A2E";
+static const char *highlightfgcolor = "#A54242";
+
+static float widthscaling = 1.5;
+static float heightscaling = 2.0;
+
